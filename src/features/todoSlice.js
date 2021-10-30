fc5ce7574717dc9892e9448a077a2998dc2deb99
xinnni <79839230+xinnni@users.noreply.github.com>
2021-10-30 23:55:57 +0900

    create redux and fix ui

---
+++ src/features/todoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todoList: [],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    saveTodo: (state, action) => {
+      state.todoList.push(action.payload);
+    },
+  },
+});
+
+export const {} = todoSlice.actions;
+export default todoSlice.reducer;
